99827f4eaa390cc15e9abbdadda3e9e5d85a6dd6
jotape02 <jpc5626@gmail.com>
2021-01-25 23:22:30 -0300

    Try 05

--- api/index.js
+++ api/index.js
@@ -3,6 +3,18 @@ const nodemailer = require('nodemailer');
 const SMTP_CONFIG = require('../src/config/smtp');
 
 module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
   const transport = nodemailer.createTransport({
     host: SMTP_CONFIG.host,
     port: SMTP_CONFIG.port,
