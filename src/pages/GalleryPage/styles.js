1f998c758fc0641be1848c676a9ce17af2f80bb6
jpc5626@gmail.com <jpc5626@gmail.com>
2022-03-16 00:11:54 -0300

    add: adding new photos to the gallery

--- src/pages/GalleryPage/styles.js
+++ src/pages/GalleryPage/styles.js
@@ -35,3 +35,32 @@ export const Styles = styled.div`
     background-color: var(--primary);
   }
 `;
+export const GardenHouseContainer = styled.div`
+  width: 100%;
+  padding: 1rem 0;
+
+  background-color: var(--primary);
+`;
+
+export const GardenHouseWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  margin-bottom: 2.5rem;
+
+  @media (min-width: 950px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+
+    width: 60%;
+    height: 800px;
+
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 2.5rem;
+  }
+`;
