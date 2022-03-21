3d3de00cf10c880cb207ec6fc53d1bdca25a0b6d
jpc5626@gmail.com <jpc5626@gmail.com>
2022-03-20 22:03:51 -0300

    fix: adjusting the gallery page

--- src/pages/GalleryPage/index.js
+++ src/pages/GalleryPage/index.js
@@ -95,39 +95,16 @@ import SummerHouse17 from '../../assets/masterServices/gardenRoom/summerHouse16.
 
 import gardenHouse01 from '../../assets/gallery/gallery-five/gardenHouse01.jpeg';
 import gardenHouse02 from '../../assets/gallery/gallery-five/gardenHouse02.jpeg';
-import gardenHouse03 from '../../assets/gallery/gallery-five/gardenHouse03.jpeg';
 import gardenHouse04 from '../../assets/gallery/gallery-five/gardenHouse04.jpeg';
-import gardenHouse05 from '../../assets/gallery/gallery-five/gardenHouse05.jpeg';
 import gardenHouse06 from '../../assets/gallery/gallery-five/gardenHouse06.jpeg';
-import gardenHouse07 from '../../assets/gallery/gallery-five/gardenHouse07.jpeg';
 import gardenHouse08 from '../../assets/gallery/gallery-five/gardenHouse08.jpeg';
-import gardenHouse09 from '../../assets/gallery/gallery-five/gardenHouse09.jpeg';
-import gardenHouse10 from '../../assets/gallery/gallery-five/gardenHouse10.jpeg';
-import gardenHouse11 from '../../assets/gallery/gallery-five/gardenHouse11.jpeg';
 import gardenHouse12 from '../../assets/gallery/gallery-five/gardenHouse12.jpeg';
-import gardenHouse13 from '../../assets/gallery/gallery-five/gardenHouse13.jpeg';
-import gardenHouse14 from '../../assets/gallery/gallery-five/gardenHouse14.jpeg';
 import gardenHouse15 from '../../assets/gallery/gallery-five/gardenHouse15.jpeg';
-import gardenHouse16 from '../../assets/gallery/gallery-five/gardenHouse16.jpeg';
-import gardenHouse17 from '../../assets/gallery/gallery-five/gardenHouse17.jpeg';
-import gardenHouse18 from '../../assets/gallery/gallery-five/gardenHouse18.jpeg';
-import gardenHouse19 from '../../assets/gallery/gallery-five/gardenHouse19.jpeg';
-import gardenHouse20 from '../../assets/gallery/gallery-five/gardenHouse20.jpeg';
-import gardenHouse21 from '../../assets/gallery/gallery-five/gardenHouse21.jpeg';
 import gardenHouse22 from '../../assets/gallery/gallery-five/gardenHouse22.jpeg';
 import gardenHouse23 from '../../assets/gallery/gallery-five/gardenHouse23.jpeg';
-import gardenHouse24 from '../../assets/gallery/gallery-five/gardenHouse24.jpeg';
-import gardenHouse25 from '../../assets/gallery/gallery-five/gardenHouse25.jpeg';
-import gardenHouse26 from '../../assets/gallery/gallery-five/gardenHouse26.jpeg';
-import gardenHouse27 from '../../assets/gallery/gallery-five/gardenHouse27.jpeg';
-import gardenHouse28 from '../../assets/gallery/gallery-five/gardenHouse28.jpeg';
-import gardenHouse29 from '../../assets/gallery/gallery-five/gardenHouse29.jpeg';
-import gardenHouse30 from '../../assets/gallery/gallery-five/gardenHouse30.jpeg';
-import gardenHouse31 from '../../assets/gallery/gallery-five/gardenHouse31.jpeg';
 import gardenHouse32 from '../../assets/gallery/gallery-five/gardenHouse32.jpeg';
 import gardenHouse33 from '../../assets/gallery/gallery-five/gardenHouse33.jpeg';
 import gardenHouse34 from '../../assets/gallery/gallery-five/gardenHouse34.jpeg';
-import gardenHouse35 from '../../assets/gallery/gallery-five/gardenHouse35.jpeg';
 import gardenHouse36 from '../../assets/gallery/gallery-five/gardenHouse36.jpeg';
 import gardenHouse37 from '../../assets/gallery/gallery-five/gardenHouse37.jpeg';
 import gardenHouse38 from '../../assets/gallery/gallery-five/gardenHouse38.jpeg';
@@ -135,9 +112,8 @@ import gardenHouse39 from '../../assets/gallery/gallery-five/gardenHouse39.jpeg'
 import gardenHouse40 from '../../assets/gallery/gallery-five/gardenHouse40.jpeg';
 import gardenHouse41 from '../../assets/gallery/gallery-five/gardenHouse41.jpeg';
 import gardenHouse42 from '../../assets/gallery/gallery-five/gardenHouse42.jpeg';
-import gardenHouse43 from '../../assets/gallery/gallery-five/gardenHouse43.jpeg';
 
-import { Styles, GardenHouseContainer, GardenHouseWrapper } from './styles';
+import { Wrapper, GalleryContainer } from './styles';
 
 import BackgroundGallery from './BackgroudGallery';
 import Title from '../../components/Title';
@@ -501,85 +477,24 @@ const images04 = [
     thumbnail: gardenHouse02,
   },
   {
-    original: gardenHouse03,
-    thumbnail: gardenHouse03,
-  },
-  {
     original: gardenHouse04,
     thumbnail: gardenHouse04,
-  },
-  {
-    original: gardenHouse05,
-    thumbnail: gardenHouse05,
   },
   {
     original: gardenHouse06,
     thumbnail: gardenHouse06,
   },
   {
-    original: gardenHouse07,
-    thumbnail: gardenHouse07,
-  },
-  {
     original: gardenHouse08,
     thumbnail: gardenHouse08,
-  },
-  {
-    original: gardenHouse09,
-    thumbnail: gardenHouse09,
-  },
-  {
-    original: gardenHouse10,
-    thumbnail: gardenHouse10,
-  },
-  {
-    original: gardenHouse11,
-    thumbnail: gardenHouse11,
   },
   {
     original: gardenHouse12,
     thumbnail: gardenHouse12,
   },
   {
-    original: gardenHouse13,
-    thumbnail: gardenHouse13,
-  },
-  {
-    original: gardenHouse14,
-    thumbnail: gardenHouse14,
-  },
-  {
     original: gardenHouse15,
     thumbnail: gardenHouse15,
-  },
-  {
-    original: gardenHouse16,
-    thumbnail: gardenHouse16,
-  },
-  {
-    original: gardenHouse17,
-    thumbnail: gardenHouse17,
-  },
-  {
-    original: gardenHouse01,
-    thumbnail: gardenHouse01,
-  },
-  {
-    original: gardenHouse18,
-    thumbnail: gardenHouse18,
-  },
-  {
-    original: gardenHouse19,
-    thumbnail: gardenHouse19,
-  },
-
-  {
-    original: gardenHouse20,
-    thumbnail: gardenHouse20,
-  },
-  {
-    original: gardenHouse21,
-    thumbnail: gardenHouse21,
   },
   {
     original: gardenHouse22,
@@ -588,42 +503,6 @@ const images04 = [
   {
     original: gardenHouse23,
     thumbnail: gardenHouse23,
-  },
-  {
-    original: gardenHouse24,
-    thumbnail: gardenHouse24,
-  },
-  {
-    original: gardenHouse25,
-    thumbnail: gardenHouse25,
-  },
-  {
-    original: gardenHouse26,
-    thumbnail: gardenHouse26,
-  },
-  {
-    original: gardenHouse27,
-    thumbnail: gardenHouse27,
-  },
-  {
-    original: gardenHouse28,
-    thumbnail: gardenHouse28,
-  },
-  {
-    original: gardenHouse01,
-    thumbnail: gardenHouse01,
-  },
-  {
-    original: gardenHouse29,
-    thumbnail: gardenHouse29,
-  },
-  {
-    original: gardenHouse30,
-    thumbnail: gardenHouse30,
-  },
-  {
-    original: gardenHouse31,
-    thumbnail: gardenHouse31,
   },
   {
     original: gardenHouse32,
@@ -638,8 +517,12 @@ const images04 = [
     thumbnail: gardenHouse34,
   },
   {
-    original: gardenHouse35,
-    thumbnail: gardenHouse35,
+    original: gardenHouse40,
+    thumbnail: gardenHouse40,
+  },
+  {
+    original: gardenHouse39,
+    thumbnail: gardenHouse39,
   },
   {
     original: gardenHouse36,
@@ -654,14 +537,6 @@ const images04 = [
     thumbnail: gardenHouse38,
   },
   {
-    original: gardenHouse39,
-    thumbnail: gardenHouse39,
-  },
-  {
-    original: gardenHouse40,
-    thumbnail: gardenHouse40,
-  },
-  {
     original: gardenHouse41,
     thumbnail: gardenHouse41,
   },
@@ -669,56 +544,48 @@ const images04 = [
     original: gardenHouse42,
     thumbnail: gardenHouse42,
   },
-  {
-    original: gardenHouse43,
-    thumbnail: gardenHouse43.jpeg,
-  },
 ];
 
 function GalleryPage() {
   return (
-    <Styles>
+    <>
       <BackgroundGallery />
       <Title title="A collection of images from our services" />
 
-      <GardenHouseContainer>
-        <div className="gallery-container">
+      <Wrapper>
+        <GalleryContainer>
           <h2>Garden office and gym house</h2>
           <ImageGallery items={images04} />
-        </div>
-      </GardenHouseContainer>
+        </GalleryContainer>
 
-      <div className="gallery-box">
-        <div className="gallery-container">
+        <GalleryContainer>
           <h2>Painting and decoration</h2>
           <ImageGallery items={images03} />
-        </div>
+        </GalleryContainer>
 
-        <div className="gallery-container">
+        <GalleryContainer>
           <h2>Summer houses</h2>
           <ImageGallery items={summerHouseImages} />
-        </div>
-      </div>
+        </GalleryContainer>
 
-      <div className="gallery-box">
-        <div className="gallery-container">
+        <GalleryContainer>
           <h2>Bathroom renovation</h2>
           <ImageGallery items={images0} />
-        </div>
+        </GalleryContainer>
 
-        <div className="gallery-container">
+        <GalleryContainer>
           <h2>Wood flooring and tile installation</h2>
           <ImageGallery items={images01} />
-        </div>
+        </GalleryContainer>
 
-        <div className="gallery-container">
+        <GalleryContainer>
           <h2>Fence erecting</h2>
           <ImageGallery items={images02} />
-        </div>
-      </div>
+        </GalleryContainer>
+      </Wrapper>
 
       <UpperFooter />
-    </Styles>
+    </>
   );
 }
 
